5b71238f9304503805ffbe0a5c105fdd1e0e3ab4
NothigBox <46972134+NothigBox@users.noreply.github.com>
2019-01-24 12:46:00 -0500

    Add files via upload

---
+++ Desafio 01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalePuesOme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario en $COP: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            double smlv = 828116;
+
+            if (salario < (smlv * 2))
+            {
+                Console.WriteLine("Su tarifa a pagar es la A");
+            }
+
+            if (salario >= (smlv * 2) && salario < (smlv * 4))
+            {
+                Console.WriteLine("Su tarifa a pagar es la B");
+            }
+
+            if (salario >= (smlv * 4))
+            {
+                Console.WriteLine("Su tarifa a pagar es la C");
+            }
+        }
+    }
+}
